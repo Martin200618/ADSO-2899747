813d697555abfbd253cd5f7641467fc60428a242
Jesús Ariel González Bonilla <jesus.gonzalez@jikkosoft.com>
2025-09-02 12:05:47 -0500

    material - week 4

--- 04-week/01-session/01-material/Backend/FlightManagementSystem/src/main/java/com/SENA/FlightManagementSystem/AircraftManagement/Entity/Arcraft.java
+++ 04-week/01-session/01-material/Backend/FlightManagementSystem/src/main/java/com/SENA/FlightManagementSystem/AircraftManagement/Entity/Arcraft.java
@@ -1,4 +1,11 @@
 package com.SENA.FlightManagementSystem.AircraftManagement.Entity;
 
-public class Arcraft {
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="aircraft")
+@Schema(name = "aircraft_managementt")
+public class Arcraft extends ABaseEntity{
 }
